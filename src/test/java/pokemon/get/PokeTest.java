a64c979e903c2ee719d293aa74431d4903aaed62
JhonySr9 <120752443+JhonySr9@users.noreply.github.com>
2024-12-08 17:48:44 -0500

    Jenkins configured to work with Karte DSL

--- src/test/java/pokemon/get/PokeTest.java
+++ src/test/java/pokemon/get/PokeTest.java
@@ -1,7 +1,10 @@
 package pokemon.get;
 
 import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class PokeTest {
 
     @Karate.Test
